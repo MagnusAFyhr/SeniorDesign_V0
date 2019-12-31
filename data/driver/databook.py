27b59f4b9f2cb25ffdbe7d8c85119983bd333514
Magnus Fyhr <magnus.fyhr@marquette.edu>
2019-12-31 12:35:14 -0600

    Preliminary work for databook and csv data parsing and formatting classes
    Signed-off-by: Magnus Fyhr <magnus.fyhr@marquette.edu>

--- data/driver/databook.py
+++ data/driver/databook.py
@@ -1,5 +1,20 @@
 
 
+import pandas
+
+
 class DataBook:
-    def __init__(self):
-        pass
+    def __init__(self, ticker, depth):
+        self.live = False
+        self.book = []
+        self.launch()
+
+    def launch(self):
+        self.live = True
+
+    def step(self):
+        if not self.live:
+            return
+
+    def close(self):
+        self.live = False
