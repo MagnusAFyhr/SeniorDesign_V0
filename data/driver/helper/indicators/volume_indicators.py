3ef529a106220beb08e7aa3e784191ab73c33e4b
RaazKH <serxtdweia9r@gmail.com>
2020-02-03 12:46:32 -0600

    Added: - momentum_indicators.py - overlap_studies.py
    Completed:
    - volatility_indicators.py
    - volume_indicators.py
    
    Notes:
    Added newline at EOF
    Nothing has been tested

---
+++ data/driver/helper/indicators/volume_indicators.py
@@ -0,0 +1,19 @@
+"""
+http://mrjbq7.github.io/ta-lib/func_groups/volume_indicators.html
+"""
+import talib as ta
+
+def AD(raw_df):
+    # Chaikin A/D Line
+    # extract necessary data from raw dataframe (high, low, close, volume)
+    return ta.ATR(raw_df.High.values, raw_df.Low.values, raw_df.Close.values, raw_df.Volume.values)
+
+def ADOSC(raw_df, fastperiod=3, slowperiod=10):
+    # Chaikin A/D Oscillator
+    # extract necessary data from raw dataframe (high, low, close, volume)
+    return ta.ADOSC(raw_df.High.values, raw_df.Low.values, raw_df.Close.values, raw_df.Volume.values, fastperiod, slowperiod)
+
+def OBV(raw_df):
+    # On Balance Volume
+    # extract necessary data from raw dataframe (close, volume)
+    return ta.OBV(raw_df.Close.values, raw_df.Volume.values)
